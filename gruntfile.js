f3f6cea034cfec0708405f4c1305f1fd5a6fd1ec
Dane Odekirk <dizzzy@uw.edu>
2013-11-08 15:16:01 -0800

    initial backbone.js version

---
+++ gruntfile.js
@@ -0,0 +1,98 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jade: {
+      compile: {
+        options: {
+          data: {
+            debug: false
+          }
+        },
+        files: {
+          "index.html": ["templates/*.jade"]
+        }
+      }
+    },
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        libraries: [
+          'js/jquery.js',
+          'js/underscore.js',
+          'js/backbone.js',
+          'js/jquery.transit.js'
+        ],
+        custom: [
+          'js/background.js'
+        ],
+        src: [ '<%= concat.dist.libraries %>', '<%= concat.dist.custom %>' ],
+        dest: 'js/site.dev.js'
+      }
+    },
+    less: {
+      development: {
+        options: {
+          cleancss: true,
+          paths: ["assets/css"]
+        },
+        files: {
+          "site.css": "less/site.less"
+        }
+      },
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today() %> */\n'
+      },
+      dist: {
+        files: {
+          'site.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    jshint: {
+      files: [ 'Gruntfile.js', '<%= concat.dist.custom %>' ],
+      options: {
+        asi: true,
+        smarttabs: true,
+        laxcomma: true,
+        lastsemic: true,
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= concat.dist.src %>', 'templates/*.jade', 'less/*.less' ],
+      tasks: ['default']
+    },
+    notify: {
+      watch: {
+        options: {
+          title: 'Task Complete',
+          message: 'Done'
+        }
+      }
+    }
+  });
+
+
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-notify');
+  
+
+  grunt.registerTask('default', ['jade', 'concat', 'less', 'uglify', 'notify']);
+
+};
---
+++ js/background.js
@@ -0,0 +1,86 @@
+jQuery(function($) {
+
+  var SplashModel = Backbone.Model.extend({})
+
+  var SplashCollection = Backbone.Collection.extend({
+  
+    model: SplashModel,
+
+    url: 'site.json',
+
+   initialize: function( options ) {
+      this.fetch();
+    }
+
+  })
+
+  var Splash = Backbone.View.extend({
+
+    DURATION: 700,
+
+    template: _.template( $('#info-template').html() ),
+
+    events: {
+      'click a span' : 'info',
+      'click span.close' : 'close',
+      'click a.tools' : 'showtools',
+      'mouseover a.project' : 'changeColor'        
+    },
+
+    initialize: function() {
+      this.$bg    = $('.background')
+      this.$info  = $('#info')
+      this.$tools = $('#tools')
+    },
+
+    info: function (){
+      this.$bg.fadeOut( _.bind( this.render, this ))
+      return false;
+    },
+
+    render: function(){
+      var info = _.pluck( this.collection.toJSON(), this.$target.data().site )
+      this.hideTools();
+      this.$info.html( this.template( _.first( info ) ) ).fadeIn()
+    },
+
+    changeColor: function( e ) {
+      this.$target = $(e.target)
+      this.setTextColor()
+      this.$bg.transit({ backgroundColor: this.color(), duration: this.DURATION, queue:false } )
+    },
+
+    setTextColor: function() {
+      this.$target.siblings().css({ color: 'inherit' } )        
+      this.$target.css({color: this.color() })
+    },
+
+    color: function() {
+      return this.$target.data().color;
+    },
+
+    close: function() {
+      this.$info.fadeOut( _.bind( this.showSplash, this ) ) 
+    },
+
+    showSplash: function() {
+      this.$bg.fadeIn() 
+    },
+
+    showtools: function() {
+      this.$tools.toggle() 
+    },
+
+    hideTools: function() {
+      this.$tools.hide()            
+    }
+  
+  })
+
+  $(document).ready(function() {
+    var splashcollection = new SplashCollection()
+      , splash =  new Splash({ el: $('.container'), collection : splashcollection });
+  })
+
+
+});
