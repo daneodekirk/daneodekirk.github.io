c6381c2f771bba5b4f9f483fad8a0ae820059cdb
Dane Odekirk <dizzzy@uw.edu>
2014-02-20 15:02:21 -0800

    cleanup

--- site.dev.js
+++ site.dev.js
@@ -12044,34 +12044,20 @@ if ( typeof window === "object" && typeof window.document === "object" ) {
       updateURL: false,                
       loop: false,                     
       keyboard: true,                  
-      responsiveFallback: false        
+      responsiveFallback: false
     },
   
     initialize : function( options ) {
-      _.bindAll(this, 'before', 'after', 'slide2')
-      this.options = _.defaults(this.options, {beforeMove: this.before, afterMove: this.after } )
+      _.bindAll(this, 'before' )
+      this.options = _.defaults(this.options, { beforeMove: this.before } )
       this.$el.onepage_scroll( this.options )
     },
 
     before : function( index ) {
       var section = this.$('section').eq(index-1).find('.inner')
       section.css({marginTop: -section.height()/4})
-//      this.$('section').eq(index-1).find('img').transit({ marginTop : 0 }, 1300 )
-    },
-
-    after : function( index ) {
-    },
-
-    slidebefore2 : function() {
-    },
-
-    slide2 : function() {
-      var stop = this.$('section.two div.wrapper')
-    },
-  
-    slide3 : function() {
-    
     }
+
   })
 
   var slide = new Slides;
